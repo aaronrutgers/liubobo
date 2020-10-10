1b9f1a135d0390af20303d6af1ba1f3a94bb39fd
aaronrutgers <aaron.now.coder@gmail.com>
2020-10-10 10:54:59 -0400

    Revert "3-2"
    This reverts commit 402c51f8

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,37 +1,6 @@
 #include <iostream>
-#include <cmath>
-#include "util.h"
-
-using namespace std;
-
-template<typename T>
-int binarySearch(T arr[], int n, T target) {
-    int l = 0, r = n - 1; // search in [l...r]
-    while (l <= r) {
-        int mid = (l + r) / 2;
-        if (arr[mid] == target) {
-            return mid;
-        }
-        if (target > arr[mid]) {
-            l = mid + 1; // target in [mid+1...r]
-        } else {
-            r = mid - 1; // target in [l...mid-1]
-        }
-    }
-    return -1;
-}
 
 int main() {
-    int n = pow(10, 7);
-    int* data = MyUtil::generateOrderedArray(n);
-
-    clock_t startTime = clock();
-    for(int i = 0 ; i < n ; i ++)
-        assert(i == binarySearch(data, n, i));
-    clock_t endTime = clock();
-
-    cout << "Binary Search test complete." << endl;
-    cout << "Time cost: " << double(endTime - startTime) / CLOCKS_PER_SEC << " s" << endl;
-
+    std::cout << "Hello, World!222" << std::endl;
     return 0;
 }
--- fiveSum.cpp
+++ fiveSum.cpp
@@ -2,25 +2,8 @@
 
 using namespace std;
 
-template<typename T>
-int binarySearch(T arr[], int n, T target) {
-    int l = 0, r = n - 1; // search in [l...r]
-    while (l <= r) {
-        int mid = (l + r) / 2;
-        if (arr[mid] == target) {
-            return mid;
-        }
-        if (target > arr[mid]) {
-            l = mid + 1; // target in [mid+1...r]
-        } else {
-            r = mid - 1; // target in [l...mid-1]
-        }
-    }
-    return -1;
-}
-
 int main() {
-    int array[5] = {1, 2, 3, 4, 5};
-    cout << binarySearch(array, sizeof(array), 5) << endl;
+    std::cout << "Hello, World!555" << std::endl;
+
     return 0;
 }

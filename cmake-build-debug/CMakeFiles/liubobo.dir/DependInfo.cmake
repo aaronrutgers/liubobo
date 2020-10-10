402c51f89fc928dcda024a8cb220345c17bd1a11
aaronrutgers <aaron.now.coder@gmail.com>
2020-10-10 10:52:37 -0400

    3-2

--- cmake-build-debug/CMakeFiles/liubobo.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/liubobo.dir/DependInfo.cmake
@@ -4,9 +4,9 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/mnt/c/Users/lican/Downloads/liubobo/binarySearch.cpp" "/mnt/c/Users/lican/Downloads/liubobo/cmake-build-debug/CMakeFiles/liubobo.dir/binarySearch.cpp.o"
+  "/Users/canli/Downloads/liubobo/binarySearch.cpp" "/Users/canli/Downloads/liubobo/cmake-build-debug/CMakeFiles/liubobo.dir/binarySearch.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
--- fiveSum.cpp
+++ fiveSum.cpp
@@ -2,8 +2,25 @@
 
 using namespace std;
 
-int main() {
-    std::cout << "Hello, World!555" << std::endl;
+template<typename T>
+int binarySearch(T arr[], int n, T target) {
+    int l = 0, r = n - 1; // search in [l...r]
+    while (l <= r) {
+        int mid = (l + r) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        }
+        if (target > arr[mid]) {
+            l = mid + 1; // target in [mid+1...r]
+        } else {
+            r = mid - 1; // target in [l...mid-1]
+        }
+    }
+    return -1;
+}
 
+int main() {
+    int array[5] = {1, 2, 3, 4, 5};
+    cout << binarySearch(array, sizeof(array), 5) << endl;
     return 0;
 }

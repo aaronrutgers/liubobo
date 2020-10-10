395186e163bbeb196f5f7f1001a073093e56e38a
aaronrutgers <aaron.now.coder@gmail.com>
2020-10-10 11:08:55 -0400

    3-2

--- fiveSum.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    std::cout << "Hello, World!555" << std::endl;
-
-    return 0;
-}
--- fourSum.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!444" << std::endl;
-    return 0;
-}
